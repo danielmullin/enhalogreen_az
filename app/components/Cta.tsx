66cb10cc9311b39f743dcd5a2bca197a4afe5a11
Daniel Mullin <email@danielmullin.com>
2023-10-24 17:24:53 +0100

    2ad0b8b7d9d8968ecbdd657582562d91826ad419

--- app/components/Cta.tsx
+++ app/components/Cta.tsx
@@ -1,4 +1,5 @@
 import { Link } from '@remix-run/react';
+import { useState } from 'react';
 
 export interface CtaProps {
 	linkTo: string;
@@ -8,14 +9,21 @@ export interface CtaProps {
 
 const Cta = (props: CtaProps): React.ReactElement => {
 	const { linkTo, text, type } = props;
+	const [ctaState, setCtaState] = useState({ disabled: false, grayscale: 'bg-white' });
+
+	const handleCtaClick = () => {
+		setCtaState({ disabled: true, grayscale: 'grayscale bg-gray-100' });
+	};
 
 	return (
-		<Link to={linkTo} className="w-full text-center">
+		<Link to={linkTo} className='w-full text-center'>
 			<button
+				disabled={ctaState.disabled}
+				onClick={handleCtaClick}
 				className={
 					type === 'primary'
-						? 'relative z-10 w-full max-w-430 rounded-half bg-white px-3 py-2 text-xl text-button-green sm:py-4'
-						: 'inner-border-2 inner-border-white relative z-10 w-full max-w-430 rounded-half px-3 py-2 text-xl sm:py-4'
+						? `relative z-10 w-full max-w-430 rounded-half px-3 py-2 text-xl text-button-green sm:py-4 ${ctaState.grayscale}`
+						: `relative z-10 w-full max-w-430 rounded-half px-3 py-2 text-xl inner-border-2 inner-border-white sm:py-4 ${ctaState.grayscale}`
 				}
 			>
 				{text}
--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -12,47 +12,46 @@ export const loader = async ({}) => {
 
 export default function Projects() {
 	const { subProjects } = useLoaderData<typeof loader>();
+
 	const content = require('app/content/projects.json');
 	let optimisticProjectId = subProjects[0].uuid;
 
 	return (
-		<div className="min-h-70 sm:min-h-80 mt-110 px-8 pt-8 sm:mx-auto sm:max-w-screen-lg">
-			<h1 className="mb-8 text-3xl leading-none ">{content.h1}</h1>
-			<section className="mb-4 sm:mb-20">
-				<div className="border-b-2 border-black pb-4 sm:pb-16">
-					<p className="mb-4 text-2xl font-light leading-tight sm:w-3/5">{content.p[0]}</p>
+		<div className='mt-110 min-h-70 px-8 pt-8 sm:mx-auto sm:min-h-80 sm:max-w-screen-lg'>
+			<h1 className='mb-8 text-3xl leading-none '>{content.h1}</h1>
+			<section className='mb-4 sm:mb-20'>
+				<div className='border-b-2 border-black pb-4 sm:pb-16'>
+					<p className='mb-4 text-2xl font-light leading-tight sm:w-3/5'>{content.p[0]}</p>
 					<select
-						className="text-md mt-2 box-border inline-flex h-10 w-full items-center justify-center rounded-xl pl-2 focus:outline-none sm:h-14"
+						className='text-md mt-2 box-border inline-flex h-10 w-full items-center justify-center rounded-xl pl-2 focus:outline-none sm:h-14'
 						defaultValue={optimisticProjectId}
-						id="uuid"
-						name="uuid"
+						id='uuid'
+						name='uuid'
 					>
-						{subProjects.map((subProject: SubProject) => {
-							return <option value={subProject.uuid}>{subProject.name}</option>;
-						})}
+						{subProjects.map((subProject: SubProject) => (
+							<option key={subProject.uuid} value={subProject.uuid}>
+								{subProject.name}
+							</option>
+						))}
 					</select>
 				</div>
 			</section>
-			<section className="mb-4 sm:mb-20">
-				<div className="border-b-2 border-black pb-6 sm:pb-16">
-					<div className="sm:flex sm:items-center">
-						<p className="mb-4 text-2xl font-light leading-tight sm:w-3/5">{content.p[1]}</p>
-						<div className="sm:w-2/5 sm:text-right">
-							<Cta
-								linkTo={`/offset-products?subProjectId=${optimisticProjectId}`}
-								text={content.cta.offsetYourProducts}
-								type="primary"
-							/>
+			<section className='mb-4 sm:mb-20'>
+				<div className='border-b-2 border-black pb-6 sm:pb-16'>
+					<div className='sm:flex sm:items-center'>
+						<p className='mb-4 text-2xl font-light leading-tight sm:w-3/5'>{content.p[1]}</p>
+						<div className='sm:w-2/5 sm:text-right'>
+							<Cta linkTo={`/offset-products?subProjectId=${optimisticProjectId}`} text={content.cta.offsetYourProducts} type='primary' />
 						</div>
 					</div>
 				</div>
 			</section>
-			<section className="sm:mx-auto sm:max-w-screen-lg">
-				<div className="pb-6 sm:pb-16">
-					<div className="sm:flex sm:items-center">
-						<p className="mb-4 text-2xl font-light leading-tight sm:w-3/5">{content.p[2]}</p>
-						<div className="sm:w-2/5 sm:text-right">
-							<Cta linkTo={`/offset-products?subProjectId=${optimisticProjectId}`} text={content.cta.offsetYourCarbon} type="primary" />
+			<section className='sm:mx-auto sm:max-w-screen-lg'>
+				<div className='pb-6 sm:pb-16'>
+					<div className='sm:flex sm:items-center'>
+						<p className='mb-4 text-2xl font-light leading-tight sm:w-3/5'>{content.p[2]}</p>
+						<div className='sm:w-2/5 sm:text-right'>
+							<Cta linkTo={`/offset-products?subProjectId=${optimisticProjectId}`} text={content.cta.offsetYourCarbon} type='primary' />
 						</div>
 					</div>
 				</div>
