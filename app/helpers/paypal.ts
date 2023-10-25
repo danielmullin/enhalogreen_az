260de65f12c2f17f54ef53711ed21cce9600c5fd
Daniel Mullin <email@danielmullin.com>
2023-10-25 19:20:40 +0100

    45ccf432b21337088e5fdb0afd1eaa061cc98675

--- app/helpers/paypal.ts
+++ app/helpers/paypal.ts
@@ -5,11 +5,12 @@ export async function  generateAccessToken(){
 		}
 	//const auth = Buffer.from(c).toString('base64'),
 	const auth = Buffer.from(`${process.env.PAYPAL_CLIENT_ID}:${process.env.PAYPAL_APP_SECRET}`).toString('base64'),
-		response = await fetch(`${process.env.PAYPAL_BASE }/v1/oauth2/token`, {
+		response = await fetch(`${process.env.PAYPAL_BASE}/v1/oauth2/token`, {
 			method: 'POST',
 			body: 'grant_type=client_credentials',
 			headers: {
-				'Authorization': `, Basic, ${ auth } `
+				'Authorization': `Basic, ${auth} `
+				//'Authorization': 'Basic,qQVh5UmpiVklEX3RySnAzc291MjZQN3NTTGI4U2drZVNmWHpGa1RpbmFoLWJtZzhKLVo3WFkyX21NU1NtNXlQbmpUQWRETFZPcWFOaU16Rk46RUdwY00xYWlPc182b3ZEYjJCYVd1MHJHdFM1eTg4NE5ZMTJ1UDJ6NENRZFU0NWVja1ZXN1ZwWU82Nk5VUENTRS1hb2ljWmx0ZkRYTV81SDU=
 			},
 		});
 		const data = await response.json();
